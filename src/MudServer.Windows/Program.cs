f2b6f1d7a00846395133b6d21886102c625a0ca2
Johnathon Sullinger <Johnathon@sullinger.us>
2015-08-22 15:47:52 -0700

    Added Windows server for the Server Adapter.

---
+++ src/MudServer.Windows/Program.cs
@@ -0,0 +1,54 @@
+﻿using MudDesigner.MudEngine.Actors;
+using MudDesigner.MudEngine.MessageBrokering;
+using System;
+using System.Threading;
+
+namespace MudDesigner.MudEngine.Networking
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            SetupMessageBrokering();
+
+            var bootstrap = new Bootstrap();
+            bool startupCompleted = false;
+            bootstrap.Initialize((game, server) =>
+            {
+                // Start the game loop.
+                while (game.IsRunning)
+                {
+                    if (!startupCompleted)
+                    {
+                        startupCompleted = true;
+                    }
+
+                    Thread.Sleep(1);
+                }
+            });
+
+            while (!startupCompleted)
+            {
+                Thread.Sleep(1);
+            }
+        }
+
+        static void SetupMessageBrokering()
+        {
+            MessageBrokerFactory.Instance.Subscribe<InfoMessage>(
+                (msg, subscription) => Console.WriteLine(msg.Content));
+
+            MessageBrokerFactory.Instance.Subscribe<GameMessage>(
+                (msg, subscription) => Console.WriteLine(msg.Content));
+
+            MessageBrokerFactory.Instance.Subscribe<PlayerCreatedMessage>(
+                (msg, sub) => Console.WriteLine("Player connected."));
+
+            MessageBrokerFactory.Instance.Subscribe<PlayerDeletionMessage>(
+                (msg, sub) =>
+                {
+                    Console.WriteLine("Player disconnected.");
+                });
+        }
+    }
+}
